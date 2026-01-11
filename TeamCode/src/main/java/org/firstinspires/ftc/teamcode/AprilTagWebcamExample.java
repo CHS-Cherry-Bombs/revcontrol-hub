3d040edb68c3f68f563a4ab8497fc36d67e1dc80
JIMMY-324 <jimmysanchez1080@gmail.com>
2026-01-10 16:21:39 -0800

    adding telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
@@ -7,12 +7,11 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 @Autonomous
 public class AprilTagWebcamExample extends OpMode {
-
     AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
 
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello", "April Tag");
         aprilTagWebcam.init(hardwareMap, telemetry);
     }
 
@@ -21,6 +20,21 @@ public class AprilTagWebcamExample extends OpMode {
         aprilTagWebcam.update();
         AprilTagDetection id20 = aprilTagWebcam.getTagBySpecificId(20);
         aprilTagWebcam.displayDetectionTelemetry(id20);
+
+        AprilTagDetection id21 = aprilTagWebcam.getTagBySpecificId(21);
+        aprilTagWebcam.displayDetectionTelemetry(id21);
+
+        AprilTagDetection id22 = aprilTagWebcam.getTagBySpecificId(22);
+        aprilTagWebcam.displayDetectionTelemetry(id22);
+
+        AprilTagDetection id23 = aprilTagWebcam.getTagBySpecificId(23);
+        aprilTagWebcam.displayDetectionTelemetry(id23);
+
+        AprilTagDetection id24 = aprilTagWebcam.getTagBySpecificId(24);
+        aprilTagWebcam.displayDetectionTelemetry(id24);
+
+        AprilTagDetection id25 = aprilTagWebcam.getTagBySpecificId(25);
+        aprilTagWebcam.displayDetectionTelemetry(id25);
 
     }
 }
